7cb659e0619b886c5e8bbd55d07bafa88c35f224
laadruturaj <laadruturaj@gmail.com>
2024-05-10 10:26:33 +0530

    Binary Numbers

---
+++ Day10.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+///////////////Solution/////////////////////////
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    
+    int count=0;
+    int maxcount=0;
+    
+    while(n>0){
+        
+        if(n & 1){
+            count++;
+            maxcount=std::max(maxcount,count);
+        }else{
+            count=0;
+        }
+        n>>=1;
+    }
+    cout<<maxcount;
+
+    return 0;
+}
+////////////////////////////////////////////////////////
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
